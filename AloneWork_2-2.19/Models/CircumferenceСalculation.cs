dd9c01c5d9d7ada59d912cd52ca75f8948d51437
Дмитрий Изох <izokh_dv@mail.ru>
2023-01-11 22:34:14 +0300

    Добавьте файлы проекта.

---
+++ AloneWork_2-2.19/Models/CircumferenceСalculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AloneWork_2_2._19.Models
+{
+    static class CircumferenceСalculation
+    {
+        public static double Сalculation(double r)
+        {
+            double c = 2 * Math.PI * r;
+            return c;
+        }
+    }
+}
